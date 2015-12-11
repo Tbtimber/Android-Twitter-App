1822946b68ec83175ba1abd0cd2bdb50a395a254
Tbtimber <delpierre.matthieu@gmail.com>
2015-12-11 16:41:02 +0100

    Practical 2 done !!!

---
+++ app/src/main/java/worldline/ssm/rd/ux/wltwitter/TweetFragment.java
@@ -0,0 +1,93 @@
+package worldline.ssm.rd.ux.wltwitter;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.List;
+
+import worldline.ssm.rd.ux.wltwitter.async.RetrieveTweetsAsyncTask;
+import worldline.ssm.rd.ux.wltwitter.interfaces.TweetChangeListener;
+import worldline.ssm.rd.ux.wltwitter.interfaces.TweetListener;
+import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
+import worldline.ssm.rd.ux.wltwitter.utils.PreferenceHandler;
+
+public class TweetFragment extends Fragment implements TweetChangeListener, AdapterView.OnItemClickListener{
+    private ListView mListView;
+    private RetrieveTweetsAsyncTask mAsyncTask;
+    private TweetListener mListener;
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if(activity instanceof TweetListener) {
+            mListener = (TweetListener) activity;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        final String login = PreferenceHandler.getPref("login");
+
+        if(!TextUtils.isEmpty(login)) {
+            mAsyncTask = new RetrieveTweetsAsyncTask(this);
+            mAsyncTask.execute(login);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View rootView = inflater.inflate(R.layout.fragment_tweet, container, false);
+        mListView = (ListView) rootView.findViewById(R.id.tweetsListView);
+        mListView.setOnItemClickListener(this);
+        final ProgressBar progressBar = new ProgressBar(getActivity());
+        progressBar.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+
+        mListView.setEmptyView(progressBar);
+        ViewGroup root = (ViewGroup) rootView.findViewById(R.id.tweetsRootRelativeLayout);
+        root.addView(progressBar);
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onTweetRetrieved(List<Tweet> tweets) {
+        final ArrayAdapter<Tweet> adapter = new ArrayAdapter<Tweet>(getActivity(), android.R.layout.simple_list_item_1, tweets);
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(null != mListener) {
+            final Tweet tweet = (Tweet) parent.getItemAtPosition(position);
+            mListener.onViewTweet(tweet);
+        }
+    }
+}
